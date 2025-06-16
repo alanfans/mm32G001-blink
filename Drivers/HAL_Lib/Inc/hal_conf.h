a270787629122a182d041d2eaaec39e927dd13c0
alanfans <hylc550655145@msn.cn>
2025-06-16 15:59:10 +0800

    cmake init

---
+++ Drivers/HAL_Lib/Inc/hal_conf.h
@@ -0,0 +1,53 @@
+/***********************************************************************************************************************
+    @file     reg_adc.h
+    @author   VV TEAM
+    @brief    The function of this file is to configure whether a peripheral is enabled or not.
+  **********************************************************************************************************************
+    @attention
+
+    <h2><center>&copy; Copyright(c) <2023> <MindMotion></center></h2>
+
+      Redistribution and use in source and binary forms, with or without modification, are permitted provided that the
+    following conditions are met:
+    1. Redistributions of source code must retain the above copyright notice,
+       this list of conditions and the following disclaimer.
+    2. Redistributions in binary form must reproduce the above copyright notice, this list of conditions and
+       the following disclaimer in the documentation and/or other materials provided with the distribution.
+    3. Neither the name of the copyright holder nor the names of its contributors may be used to endorse or
+       promote products derived from this software without specific prior written permission.
+
+      THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS "AS IS" AND ANY EXPRESS OR IMPLIED WARRANTIES,
+    INCLUDING, BUT NOT LIMITED TO, THE IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE ARE
+    DISCLAIMED. IN NO EVENT SHALL THE COPYRIGHT HOLDER OR CONTRIBUTORS BE LIABLE FOR ANY DIRECT, INDIRECT, INCIDENTAL,
+    SPECIAL, EXEMPLARY, OR CONSEQUENTIAL DAMAGES (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR
+    SERVICES; LOSS OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER CAUSED AND ON ANY THEORY OF LIABILITY,
+    WHETHER IN CONTRACT, STRICT LIABILITY, OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE
+    OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
+  *********************************************************************************************************************/
+
+/* Define to prevent recursive inclusion -----------------------------------------------------------------------------*/
+#ifndef __HAL_CONF_H
+#define __HAL_CONF_H
+
+/* Files includes ----------------------------------------------------------------------------------------------------*/
+#include "../Inc/hal_adc.h"
+#include "../Inc/hal_crc.h"
+#include "../Inc/hal_dbg.h"
+#include "../Inc/hal_exti.h"
+#include "../Inc/hal_flash.h"
+#include "../Inc/hal_gpio.h"
+#include "../Inc/hal_i2c.h"
+#include "../Inc/hal_iwdg.h"
+#include "../Inc/hal_misc.h"
+#include "../Inc/hal_pwr.h"
+#include "../Inc/hal_rcc.h"
+#include "../Inc/hal_spi.h"
+#include "../Inc/hal_syscfg.h"
+#include "../Inc/hal_tim.h"
+#include "../Inc/hal_uid.h"
+#include "../Inc/hal_usart.h"
+#include "mm32_device.h"
+
+
+#endif
+
